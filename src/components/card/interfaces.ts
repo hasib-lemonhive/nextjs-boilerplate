dde77931b7636f7671095bd8cbb0fafa47e5f8fc
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-02 12:20:11 +0000

    Update card interface

--- src/components/card/interfaces.ts
+++ src/components/card/interfaces.ts
@@ -1,6 +1,23 @@
+import { PortableTextBlock } from '@portabletext/types';
+
+export interface IDog {
+  name: string;
+  breed: string;
+  description: PortableTextBlock[];
+  image: {
+    url: string;
+    alt: string;
+    lqip: string;
+  };
+}
+
+export const CardImageDimensions = {
+  height: 400,
+  width: 600,
+};
+
 export interface ICard {
-  link?: string | undefined;
-  header: string;
-  paragraph: string;
+  dog: IDog;
+  link?: string;
   testId?: string;
 }
