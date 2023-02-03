751eef805793572c0dcdecb075cf71f3951300e4
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-03 05:07:52 +0000

    Update custom image interface

--- src/components/custom-image/interface.ts
+++ src/components/custom-image/interface.ts
@@ -1,13 +1,10 @@
-export interface ICustomImage {
-  src: string;
-  height: number;
-  width: number;
-  alt: string;
-  lqip?: string;
-}
-
 export interface ICustomFillImage {
   src: string;
   alt: string;
   lqip?: string;
+}
+
+export interface ICustomImage extends ICustomFillImage {
+  height: number;
+  width: number;
 }
