6175e8d8600d1bcec43e2ee80165b3c80d10a2c2
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-03 14:26:44 +0000

    Create custom portable text component

---
+++ src/components/custom-portable-text/index.tsx
@@ -0,0 +1,17 @@
+import { PortableText } from '@portabletext/react';
+import { CustomComponents } from './config';
+import { ICustomPortableText } from './interface';
+
+/**
+ * This element renders portable text according to custom
+ * blocks and schemas used in sanity's portable text.
+ * @param {object}  ICustomPortableText
+ * @returns JSX.Element
+ */
+export const CustomPortableText = ({ content }: ICustomPortableText) => {
+  return (
+    <div>
+      <PortableText value={content} components={CustomComponents} />
+    </div>
+  );
+};
---
+++ src/components/custom-portable-text/interface.ts
@@ -0,0 +1,5 @@
+import { PortableTextBlock } from '@portabletext/types';
+
+export interface ICustomPortableText {
+  content: PortableTextBlock[];
+}
