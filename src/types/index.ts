691954d7f02b45b7d0900d4774bfe43c4ccadf94
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-03 14:27:51 +0000

    Create custom types for sanity links

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
+import {
+  ISanityOptionalLink,
+  ISanityRequiredLink,
+} from 'src/backend/types/index';
 /**
  * This is an example of an enum that does not belong to a specfic component/section
  * but can be used by multiple components/sections
@@ -6,4 +10,19 @@
 export enum SectionThemeEnumGlobal {
   Light = 'Light',
   Dark = 'Dark',
+}
+
+/**
+ * Since the generated interface for 'linkType' is a string.
+ * We have to create a custom interface where we hardcode the type.
+ *
+ * This has to match the string options in the sanity backend. It is the
+ * responsibility of the project's backend devloper to update these types.
+ */
+export interface CustomOptionalLink extends ISanityOptionalLink {
+  linkType: 'internalLink' | 'externalLink';
+}
+
+export interface CustomRequiedLink extends ISanityRequiredLink {
+  linkType: 'internalLink' | 'externalLink';
 }
