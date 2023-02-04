38ac668e64157b1a38af90894a5889ee11ef9aa5
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-04 09:00:13 +0000

    Enable hotspot

--- src/backend/queries/fragments/image.ts
+++ src/backend/queries/fragments/image.ts
@@ -5,6 +5,22 @@ export const IMAGE_FRAGMENT = gql(`
     altDescription
     figcaption
     imageFile {
+      hotspot {
+        _key
+        _type
+        x
+        y
+        height
+        width
+      }
+      crop {
+        _key
+        _type
+        top
+        bottom
+        left
+        right
+      }
       asset {
         url
         metadata {
