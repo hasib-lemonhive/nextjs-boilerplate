04a7b50843219fd8e3a056a36e0c8c8d3f9666b2
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-05 09:40:21 +0000

    Update image fragment

--- src/backend/queries/fragments/image.ts
+++ src/backend/queries/fragments/image.ts
@@ -1,37 +1,6 @@
-import { gql } from '@apollo/client';
-
-export const IMAGE_FRAGMENT = gql(`
-  fragment ImageFragment on CustomImageSchema {
-    altDescription
-    figcaption
-    imageFile {
-      hotspot {
-        _key
-        _type
-        x
-        y
-        height
-        width
-      }
-      crop {
-        _key
-        _type
-        top
-        bottom
-        left
-        right
-      }
-      asset {
-        url
-        metadata {
-          dimensions {
-            height
-            width
-            aspectRatio
-          }
-          lqip
-        }
-      }
+export const imageFragment = `
+    ...,
+    imageFile{
+      asset->
     }
-  }
-`);
+`;
