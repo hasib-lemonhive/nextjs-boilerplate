42f08d7ab605ba141f56ec52e42fd1d1b36c6854
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-05 09:39:07 +0000

    Remove apollo client provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,16 @@ import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import ExampleNavbar from 'src/sections/nav-bar';
 import { inter, roboto } from 'fonts';
-import { ApolloProvider } from '@apollo/client';
-import client from 'src/backend/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
   /*Add fonts variables to all pages*/
   return (
-    <ApolloProvider client={client}>
-      <div
-        className={`${roboto.variable} ${inter.variable} text-black dark:text-slate-200 transition-colors ease-in`}
-      >
-        <ExampleNavbar />
-        <Component {...pageProps} />
-      </div>
-    </ApolloProvider>
+    <div
+      className={`${roboto.variable} ${inter.variable} text-black dark:text-slate-200 transition-colors ease-in`}
+    >
+      <ExampleNavbar />
+      <Component {...pageProps} />
+    </div>
   );
 }
 
