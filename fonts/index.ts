da480b3e75bf72ac0818c1ea2f2b2bf3727fe5d6
rabbani-lm <rabbani@lemonhive.com>
2023-02-07 08:33:17 +0000

    update font obj

--- fonts/index.ts
+++ fonts/index.ts
@@ -7,10 +7,14 @@ import { Roboto, Inter } from '@next/font/google';
 export const primary = Roboto({
   subsets: ['latin'],
   variable: '--font-primary',
+  style: ['normal', 'italic'],
   weight: ['400', '500', '700'],
+  display: 'optional',
 });
 
 export const secondary = Inter({
   subsets: ['latin'],
   variable: '--font-secondary',
+  style: ['normal'],
+  display: 'optional',
 });
