4707b958d2cc8932f97af7a622218cdbe58486ac
rabbani-lm <rabbani@lemonhive.com>
2023-02-08 09:33:51 +0000

    add icon store

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,39 @@
+import { IIconNames } from './interface';
+import { ErrorVector } from './sub-components/error';
+import { NoDataVector } from './sub-components/no-data';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+
+interface IIconStoreProps {
+  iconName: IIconNames;
+}
+
+/**
+ * @method IconStore
+ * @param {iconName} IIconStoreProps
+ * @returns {JSX.Element}
+ * @description this function will receive iconName and return icon JSX.Element(svg)
+ */
+const IconStore = ({ iconName }: IIconStoreProps) => {
+  switch (iconName) {
+    case 'error-vector': {
+      return <ErrorVector />;
+    }
+    case 'no-data-vector': {
+      return <NoDataVector />;
+    }
+    case 'arrow-left': {
+      return <FaArrowLeft />;
+    }
+    case 'arrow-right': {
+      return <FaArrowRight />;
+    }
+    case 'chevron-left': {
+      return <FaArrowLeft />;
+    }
+    case 'chevron-right': {
+      return <FaArrowRight />;
+    }
+  }
+};
+
+export default IconStore;
---
+++ src/components/icons/interface.ts
@@ -0,0 +1,5 @@
+type ArrowIcon = 'arrow-left' | 'arrow-right';
+type ChevronIcon = 'chevron-left' | 'chevron-right';
+type VectorIcon = 'error-vector' | 'no-data-vector';
+
+export type IIconNames = ArrowIcon | ChevronIcon | VectorIcon;
