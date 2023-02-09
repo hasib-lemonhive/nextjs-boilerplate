97757a77ffe49bfed91ddb3e9671bf79cbd9f250
rabbani-lm <rabbani@lemonhive.com>
2023-02-09 06:05:05 +0000

    update button interface

--- src/components/button/interface.ts
+++ src/components/button/interface.ts
@@ -1,21 +1,29 @@
 import React from 'react';
 import { IconNames } from './../icons/interface';
 
-type ColorScheme =
-  | 'primary-bg-text-white'
-  | 'secondary-bg-text-white'
-  | 'black-bg-text-white'
-  | 'white-bg-text-black';
+// type ColorScheme =
+//   | 'primary-bg-text-white'
+//   | 'secondary-bg-text-white'
+//   | 'black-bg-text-white'
+//   | 'white-bg-text-black';
+export const buttonColorSchemes = {
+  primaryBgTextWhite: 'primary-bg-text-white',
+  secondaryBgTextWhite: 'secondary-bg-text-white',
+  blackBgTextWhite: 'black-bg-text-white',
+  whiteBgTextBlack: 'white-bg-text-black',
+} as const;
+type colorSchemeKeys = keyof typeof buttonColorSchemes;
+type ColorScheme = (typeof buttonColorSchemes)[colorSchemeKeys];
 
 // type Size = 'lg' | 'md' | 'sm' | 'xs';
-export const SizeValues = {
+export const buttonSizes = {
   lg: 'lg',
   md: 'md',
   sm: 'sm',
   xs: 'xs',
 } as const;
-type sizeKeys = keyof typeof SizeValues;
-type Size = (typeof SizeValues)[sizeKeys];
+type sizeKeys = keyof typeof buttonSizes;
+type Size = (typeof buttonSizes)[sizeKeys];
 
 export interface IButton {
   title: string;
