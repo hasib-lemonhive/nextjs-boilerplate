4c239c88214b1e24af8feda05213334a435bfaad
rabbani-lm <rabbani@lemonhive.com>
2023-02-09 06:05:31 +0000

    update button storybook

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,9 +1,29 @@
 import Component from '.';
 import { Meta, StoryObj } from '@storybook/react';
 import { buttonProps } from './mock-data';
+import { buttonColorSchemes, buttonSizes } from './interface';
 
 const meta: Meta<typeof Component> = {
   component: Component,
+  argTypes: {
+    colorScheme: {
+      control: 'select',
+      options: [...Object.values(buttonColorSchemes)],
+    },
+    hoverColorScheme: {
+      control: 'select',
+      options: [...Object.values(buttonColorSchemes)],
+    },
+    size: {
+      control: 'inline-radio',
+      options: [...Object.values(buttonSizes)],
+    },
+    'data-testid': {
+      table: {
+        disable: true,
+      },
+    },
+  },
   parameters: {
     layout: 'centered',
   },
