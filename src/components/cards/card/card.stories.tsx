112f413995afd1d29598f63cc8adb4e117db9f00
rabbani-lm <rabbani@lemonhive.com>
2023-02-09 06:49:16 +0000

    update card with decorators

--- src/components/cards/card/card.stories.tsx
+++ src/components/cards/card/card.stories.tsx
@@ -5,8 +5,19 @@ import { mockDog } from './mock-data';
 const meta: Meta<typeof Component> = {
   component: Component,
   parameters: {
-    layout: 'centered',
+    layout: 'full',
   },
+  decorators: [
+    (Story) => (
+      <div className="py-10">
+        <div className="container">
+          <div className="max-w-[360px]">
+            <Story />
+          </div>
+        </div>
+      </div>
+    ),
+  ],
 };
 
 export default meta;
